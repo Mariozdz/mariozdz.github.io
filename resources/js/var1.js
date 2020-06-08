71eff1df80ffa445f8054cf93b9f6ccf5e59c06e
Mariozdz <46728059+Mariozdz@users.noreply.github.com>
2020-06-07 21:55:30 -0600

    "last commit"

--- resources/js/var1.js
+++ resources/js/var1.js
@@ -1,17 +1,33 @@
-export function var1_animation() {
-  createAssets();
+function var1_animation() {
   console.log("Variable 1 animation has begun.");
-  /*
-   * Aquí se hace la animación, preferiblemente usando anime.timeline(parameters);
-   */ 
+  var slider = $('#slider');
+  slider.val(0);
+  var timeline = anime.timeline({
+    loop: false,
+    autoplay: false,
+    easing: 'linear',
+    update: () => slider.val(timeline.progress)
+  });
+  // BEGIN ANIMATION
+  show_var1_initial(timeline);
+  from_var1_to_main(timeline);
+  from_main_to_router(timeline);
+  from_router_to_server(timeline, "Se detectó una cantidad de calor peligrosa");
+  from_server_to_everyone(timeline);
+  // END ANIMATION
+  $(".play-button").click(timeline.play);
+  $(".pause-button").click(timeline.pause);
+  $(".restart-button").click(() => {
+    timeline.restart();
+    timeline.pause();
+    slider.val(0);
+  });
+  slider.on('input', ()=> timeline.seek(timeline.duration * (slider.val() / 100)));
 }
-function createAssets() {
-  /*
-   * Aquí uno añade el fondo y los objetos requeridos al objeto:
-   *  $("#canvas")
-   * Estos se pueden añadir usando: #("#canvas").append();
-   * Estos objetos se pueden crear usando JQuery como una tag:
-   *  var objecto = $("<tag>")
-   */
+function var1_createAssets() {
+  $('<img>', {
+    class: 'background',
+    src: 'resources/images/temperature.svg'
+  }).appendTo('#canvas');
   console.log("Created assets for variable 1.");
 }
